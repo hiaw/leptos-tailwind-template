925c582a2d9dbdeb6415c6182ab35dcad9cadf57
Daniel Chong <danielgq.chong@gmail.com>
2024-05-28 17:11:17 +1200

    adding console error panic hook

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(|| view! { <p>"Hello, world!"</p> })
 }
